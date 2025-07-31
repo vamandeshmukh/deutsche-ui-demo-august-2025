2e49537c17c70097333322655c1ff6d52d292d47
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-31 16:05:13 +0530

    ui demo

--- js/js-callbacks.js
+++ js/js-callbacks.js
@@ -1,7 +1,8 @@
 // Callbacks in javaScript 
 
-
+// =======================
 // // Problem in Async JS 
+// =======================
 // const getData = () => {
 //     console.log("getData");
 //     setTimeout(() => {
@@ -15,42 +16,81 @@
 // console.log("end");
 
 
+// =========================
 // Solution 1 - use callbacks 
+// =========================
 // pass a function as argument to another function 
 
+// const getData = (arg) => {
+//     console.log("getData");
+//     setTimeout(() => {arg({message: "data is available." }); }, 1000);
+// };
+
+// getData((data) => { console.log(data.message);});
+
+// const fun = (data) => { console.log(data.message);}
+// getData(fun);
+
+
+// // =========================
+// // Solution 2 Promise using then() -> pending = success / failure 
+// // =========================
+
+// // function that returns a promise == sends data 
+// const getData = (arg) => {
+//     console.log("getData");
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve({message: "data is available." });
+//         }, 1000);
+//     });
+// };
+
+// // function call that consumes the promise == gets data 
+// getData()
+// .then((data) => {
+//     console.log(data.message);
+// });
+
+
+
+// =========================
+// Solution 3 Promise with async / await 
+// =========================
+
+// function that returns a promise == sends data 
 const getData = (arg) => {
     console.log("getData");
-    setTimeout(
-        () => {arg({message: "data is available." }); } 
-    , 1000);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({message: "data is available." });
+        }, 1000);
+    });
 };
 
-console.log("start");
-getData(
-    (data) => { console.log(data.message);}
-);
-console.log("end");
+// function call that consumes the promise with async / await == gets data 
 
+const useData = async () => {
+    const data = await getData();
+    console.log(data.message);
+};
 
-
-
-
-
+useData();
 
 
 // fetch().then().then().catch();
 // fetch((apiUrl) => {}).then(() => {}).then(() => {}).catch(() => {});
 
-const apiUrl = "https://jsonplaceholder.typicode.com/users/1";
+// const apiUrl = "https://jsonplaceholder.typicode.com/users/1";
 
-fetch(apiUrl)
-.then((response) => { return response.json();})
-.then((data) => {
-    console.log(data.username);
-    console.log(data.nonExistingProperty);
-});
+// fetch(apiUrl)
+// .then((response) => { return response.json();})
+// .then((data) => {
+//     console.log(data.username);
+//     console.log(data.nonExistingProperty);
+// });
 
-// // shortcut syntax 
-fetch(apiUrl)
-.then(resp => resp.json())
-.then(data => console.log(data.username));
+// // // shortcut syntax 
+// fetch(apiUrl)
+// .then(resp => resp.json())
+// .then(data => console.log(data.username));
