8b71f0c615c9322172ef1925542ec926023ea9be
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-31 15:30:03 +0530

    ui demo

--- js/js-callbacks.js
+++ js/js-callbacks.js
@@ -20,11 +20,37 @@
 
 const getData = (arg) => {
     console.log("getData");
-    setTimeout(() => {
-        arg({message: "data is available." });
-    }, 1000);
+    setTimeout(
+        () => {arg({message: "data is available." }); } 
+    , 1000);
 };
 
 console.log("start");
-getData((data) => { console.log(data.message);});
+getData(
+    (data) => { console.log(data.message);}
+);
 console.log("end");
+
+
+
+
+
+
+
+
+// fetch().then().then().catch();
+// fetch((apiUrl) => {}).then(() => {}).then(() => {}).catch(() => {});
+
+const apiUrl = "https://jsonplaceholder.typicode.com/users/1";
+
+fetch(apiUrl)
+.then((response) => { return response.json();})
+.then((data) => {
+    console.log(data.username);
+    console.log(data.nonExistingProperty);
+});
+
+// // shortcut syntax 
+fetch(apiUrl)
+.then(resp => resp.json())
+.then(data => console.log(data.username));
